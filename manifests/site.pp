ce43490158c9d4adc3b5440921ed508ed83e44aa
Awor1390 <154054133+Awor1390@users.noreply.github.com>
2024-01-15 13:13:00 +0300

    Update site.pp 8

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,13 @@
-node slave1.puppet {
-  class { 'apache':
-    listen => ['80', '443'],
-    mpm    => 'prefork',
-  }
-  
-  file { '/var/www/html/index.html':
-    ensure => file,
-    content => "Привет, мир!\n",
-  }  
+node default {
+    class { '::apt':
+        update_cache => true,
+        allow_unauthenticated => on,
+    }
+
+    include apache
+}
+class apache {
+    package { ['apache2']:
+        ensure => present,
+    }
 }
