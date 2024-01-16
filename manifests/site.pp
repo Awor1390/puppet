ee71d27d89651307a9eb1b49d50d8cf9b3705465
Konstantin <pwd1390@mail.ru>
2024-01-16 22:00:41 +0300

    incluse 1

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,13 +1,6 @@
 node 'slave1.puppet' {
 
-  class { 'apache':
-    listen => ['80', '443'],
-    mpm    => 'prefork',
-  }
-  
-  file { '/var/www/html/index.html':
-    ensure => file,
-    content => "Привет, мир!\n",
-  }  
+  include common
+  include apache
   
 }
