f5bbe67447b4d9660b50d8da41df5e888e7339cd
Konstantin <pwd1390@mail.ru>
2024-01-16 22:34:31 +0300

    apach4

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,12 @@
 node 'slave1.puppet' {
 
 class { 'apache':
+  default_vhost => false,
+}
 
-  apache::vhost { 'vhost.example.com':
+apache::vhost { 'vhost.example.com':
   port    => 80,
   docroot => '/var/www/vhost',
-}
 }
 
 }
