60096f6eb41e0c5fde1b565e52bda59995e4aab5
Konstantin <pwd1390@mail.ru>
2024-01-16 23:28:27 +0300

    apache4

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,34 @@
 node 'slave1.puppet' {
 
-class { 'apache':
-  default_vhost => false,
+  class { 'apache':
+    default_vhost => false,
+  }
+
+  apache::vhost { 'vhost.static.com':
+    port    => 80,
+    docroot => '/var/www/a.example.com',
+  }
+
+  file { '/vagrant/files/index.html':
+    ensure => '/var/www/a.example.com/index.html',
+  }
+
 }
 
-apache::vhost { 'vhost.example.com':
-  port    => 80,
-  docroot => '/var/www/vhost',
-}
+
+node 'slave2.puppet' {
+
+  class { 'apache':
+    default_vhost => false,
+  }
+
+  apache::vhost { 'vhost.dynamic.com':
+    port    => 80,
+    docroot => '/var/www/b.example.com',
+  }
+
+  file { '/vagrant/files/index.php':
+    ensure => '/var/www/b.example.com/index.php',
+  }
 
 }
