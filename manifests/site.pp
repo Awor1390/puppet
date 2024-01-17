31de42dd21fe18462f9f37fe6ca29c4be2f120f4
Konstantin <pwd1390@mail.ru>
2024-01-17 11:06:32 +0300

    site2

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,40 +1,45 @@
 node 'slave1.puppet' {
 
+  #stop firewall
   exec { 'stop firewalld':
     command => 'systemctl stop firewalld',
     refresh => true,
   }
 
-
+  #init apache
   class { 'apache':
     default_vhost => false,
   }
 
+  #start apache on port 80
   apache::vhost { 'a.example.com':
     port    => 80,
     docroot => '/var/www/a.example.com',
   }
 
-  class { '::php':
-    ensure       => latest,
-    manage_repos => true,
-    fpm          => true,
-    dev          => true,
-    composer     => true,
-    pear         => true,
-    phpunit      => false,
-  }
-
+  #add stativ page as file
+  file { '/var/www/html/index.html':
+    ensure => file,
+    content => "<html>\n
+      <head>\n
+        <title>Static page</title>\n
+      </head>\n
+      <body>\n
+        <h1>Hello world</h1>\n
+      </body>\n
+    </html>\n",
+  }  
 }
-
 
 node 'slave2.puppet' {
 
+  #stop firewall
   exec { 'stop firewalld':
     command => 'systemctl stop firewalld',
     refresh => true,
   }
 
+  #init apache
   class { 'apache':
     default_vhost => false,
   }
@@ -52,6 +57,6 @@ node 'slave2.puppet' {
     composer     => true,
     pear         => true,
     phpunit      => false,
-  } 
+  }
 
 }
