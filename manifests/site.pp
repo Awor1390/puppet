94a37653d0218d58551303e1fe3dc785b2110347
Konstantin <pwd1390@mail.ru>
2024-01-21 19:18:41 +0300

    dz4-14

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,14 +2,14 @@ node 'master.puppet' {
     
   class{'nginx': }
 
-  nginx::resource::location{'/':
-    proxy => 'http://192.168.56.42' ,
-    server => 'localhost/dynamic'
+  nginx::resource::location{'/dynamic':
+    proxy => 'http://192.168.56.42/' ,
+    server => 'localhost'
   }
 
-  nginx::resource::location{'/':
-    proxy => 'http://192.168.56.41' ,
-    server => 'localhost/static'
+  nginx::resource::location{'/static':
+    proxy => 'http://192.168.56.41/' ,
+    server => 'localhost'
   }
   
 }
